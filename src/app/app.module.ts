f52f935af9d004c76bdbd7e7162fe7d2fb000811
akilav-25 <akilanvaratharajan9842@gmail.com>
2024-12-28 10:58:41 +0530

    header component changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
